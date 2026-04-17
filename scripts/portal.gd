0dad37b5576dc52cec16c4ff4f35bb9fb5be5fe6
Odelon Jhey Pacalso <odjhey@gmail.com>
2026-04-17 23:53:48 +0800

    refactor: dedupe portal a b

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -1,8 +1,8 @@
-class_name PortalA
+class_name Portal
 extends Node2D
 
 @export var game_state_node : GameState
-@export var portal_b : PortalB
+@export var portal_b : Portal
 var game_state : GameStateContext
 var entity : PortalEntity
 var entity_id : int
@@ -18,6 +18,12 @@ func _ready():
 	entity.teleport_requested.connect(on_teleport_requested)
 	game_state_node.register_node(entity_id, self)
 
+	call_deferred("complete_setup")
+
+func complete_setup():
+	if portal_b != null:
+		entity.set_portal_pair(portal_b.entity_id)
+
 func on_area_entered(_area: Area2D):
 	# todo find a way to know if its a player
 	game_state.tag_entity(&"in_player_range", entity_id)
@@ -30,8 +36,14 @@ func on_area_exited(_area: Area2D):
 	print("exited")
 
 func on_teleport_requested(p_subject: int, p_target: int):
-	var subject_node: Node2D = game_state_node.get_entity_node(p_subject)
-	var target_node: Node2D = game_state_node.get_entity_node(p_target)
+	var subject_node := game_state_node.get_entity_node(p_subject) as Node2D
+	var target_node := game_state_node.get_entity_node(p_target) as Node2D
 
-	print(subject_node.global_position, target_node.global_position)
+	if subject_node == null:
+		push_warning("Teleport subject node not found: %s" % p_subject)
+		return
+	if target_node == null:
+		push_warning("Teleport target node not found: %s" % p_target)
+		return
+
 	subject_node.global_position = target_node.global_position
